c1b9089380558288a6a5f93b3c90676fe2e95945
tinngo56 <tinngo56@iastate.edu>
2023-04-08 16:09:18 -0500

    changes

--- Assignment02/store/src/App.js
+++ Assignment02/store/src/App.js
@@ -18,11 +18,11 @@ export const App = () => {
 	  {console.log("Step 3 : in render_products ")}
       <h2 className="text-3xl font-extrabold tracking-tight text-gray-600 category-title">Products ({ProductsCategory.length})</h2>
 
-      <div className="m-6 p-3 mt-10 ml-0 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-6 xl:gap-x-10" style={{ maxHeight: '800px', overflowY: 'scroll' }}>
+      <div className="m-6 p-3 mt-10 ml-0 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-6 xl:gap-x-10" style={{ maxHeight: '700px', overflowY: 'scroll' }}>
         {/* Loop Products */}
         {ProductsCategory.map((product, index) => (
           <div key={index} className="group relative shadow-lg" >
-            <div className=" min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-60 lg:aspect-none">
+            <div className=" min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-100 lg:aspect-none">
               <img
                 alt="Product Image"
                 src={product.image}
@@ -41,18 +41,21 @@ export const App = () => {
                 <p className="mt-1 text-sm text-gray-500">Rating: {product.rating.rate}</p>
               </div>
               <p className="text-sm font-medium text-green-600">${product.price}</p>
+              
             </div>
           </div>
         ))}
       </div>
+      
     </div>
+    
   }
 
   
   function handleClick(tag){
     console.log("Step 4 : in handleClick", tag);
     let filtered = Products;
-    if (tag) {
+    if (tag && tag !== "Everything") {
       filtered = Products.filter(cat => cat.category === tag);
     }
     setProductsCategory(filtered);
@@ -60,20 +63,22 @@ export const App = () => {
   }
   
   
+  
   const handleChange = (e) => {
     setQuery(e.target.value);
     console.log("Step 6 : in handleChange, Target Value :",e.target.value,"  Query Value :",query);
     const results = ProductsCategory.filter(eachProduct => {
-    if (e.target.value === "") return ProductsCategory;
+      if (e.target.value === "") return ProductsCategory;
       return eachProduct.title.toLowerCase().includes(e.target.value.toLowerCase())
     });
     setProductsCategory(results);
   }
   
+  
   return (
     <div className="flex fixed flex-row">
 	    {console.log("Step 2 : Return App :",Products.length,ProductsCategory.length)}
-      <div className="h-screen  bg-slate-800 p-3 xl:basis-1/5" style={{ minWidth: '65%' }}>
+      <div className="h-screen  bg-slate-800 p-3 xl:basis-1/5" style={{ minWidth: '50%' }}>
         <div className="px-6 py-4">
           <h1 className="text-3xl mb-2 font-bold text-white"> Product Catalog App </h1>
           <p className="text-gray-700 text-white">
@@ -86,6 +91,7 @@ export const App = () => {
             }
           </div>
           <div className="py-10">
+            <p>Search Bar</p>
             <input type="search" value={query} onChange={handleChange} />
           </div>
         </div>
